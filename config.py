306e69fa9a97d64fd572d678f63784a1dfdec5f2
aakashv100 <vardhan.aakash1@gmail.com>
2023-11-30 19:31:50 +0400

    Update NUM_WORKERS to use os.cpu_count()

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import albumentations as A
 import cv2
 import torch
-
+import os
 from albumentations.pytorch import ToTensorV2
 # from utils import seed_everything
 
 DATASET = 'PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # seed_everything()  # If you want deterministic behavior
-NUM_WORKERS = 0
+NUM_WORKERS = os.cpu_count()
 BATCH_SIZE = 32
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
